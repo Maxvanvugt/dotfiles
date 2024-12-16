655cca661e9eff63213796dccf5e703797f91b22
Max van Vugt <max.vanvugt@opensatisfaction.nl>
2024-12-16 10:28:29 +0100

    Added lvim plugins

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -1,6 +1,5 @@
 vim.opt.relativenumber = true
 
-lvim.keys.normal_mode["s"] = "<Plug>(leap)"
 lvim.keys.normal_mode["m"] = ":write<CR>"
 
 lvim.keys.normal_mode["<TAB>"] = ":BufferLineCycleNext<CR>"
@@ -10,6 +9,7 @@ lvim.keys.normal_mode["<S-TAB>"] = ":BufferLineCyclePrev<CR>"
 
 lvim.builtin.which_key.mappings["f"] = {
   name = "Find",
+
   f = { ":Telescope find_files<cr>", "Find Files" },
   d = { ":Telescope live_grep<cr>", "Live Grep" },
   g = { ":Telescope git_status<cr>", "Live Grep" },
@@ -21,6 +21,20 @@ lvim.builtin.which_key.mappings["c"] = {
   c = { ":BufferKill<cr>", "Close Current" },
   l = { ":BufferLineCloseLeft<cr>", "Close Left" },
   r = { ":BufferLineCloseRight<cr>", "Close Right" },
+  w = { ":close<cr>", "Close Window" },
+}
+
+lvim.builtin.which_key.mappings["j"] = {
+  name = "LSP Saga",
+  f = { ":Lspsaga peek_definition<cr>", "Peek Defintion" },
+  s = { ":Lspsaga diagnostic_jump_next<cr>", "Diagnostic Jump Next" },
+  d = { ":Lspsaga hover_doc<cr>", "Hover Doc" }
+}
+
+lvim.builtin.which_key.mappings["k"] = {
+  name = "Git Signs",
+  f = { ":Gitsigns next_hunk<cr>", "Next Hunk" },
+  d = { ":Gitsigns toggle_current_line_blame<cr>", "Toggle Current Line Blame" },
 }
 
 lvim.builtin.telescope.theme = "center"
@@ -30,17 +44,34 @@ lvim.plugins = {
     'norcalli/nvim-colorizer.lua'
   },
   {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {},
+    -- stylua: ignore
+    keys = {
+      { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+      { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" }
+    },
+  },
+  {
     'ggandor/leap.nvim'
   },
   {
     'catppuccin/nvim'
+  },
+  {
+    'nvimdev/lspsaga.nvim',
+    config = function()
+      require('lspsaga').setup({
+        symbol_in_winbar = {
+          enable = false,
+        }
+      })
+    end,
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter', -- optional
+      'nvim-tree/nvim-web-devicons',     -- optional
+    },
   }
-}
 
-vim.cmd [[
-augroup kitty_mp
-    autocmd!
-    au VimLeave * :silent !kitty @ set-spacing padding=20 margin=10
-    au VimEnter * :silent !kitty @ set-spacing padding=0 margin=0
-augroup END
-]]
+}
