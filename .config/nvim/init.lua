f53841fc77cefb6367b1b1b96c7f90b828ea0b5e
Max van Vugt <max.vanvugt@opensatisfaction.nl>
2026-04-18 19:13:47 +0200

    Added: some treesitter stuff

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,19 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+vim.filetype.add({
+    pattern = {
+        [".*%.component%.html"] = "htmlangular",
+    },
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "htmlangular",
+    callback = function(ev)
+        vim.treesitter.start(ev.buf)
+    end,
+})
+
 require("core.pack")
 require("core.plugin_config")
 
@@ -26,6 +39,7 @@ vim.opt.swapfile = false
 vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
+vim.opt.fixendofline = false
 vim.opt.clipboard = "unnamedplus"
 vim.opt.hidden = true
 vim.opt.termguicolors = true
--- .config/nvim/lsp/angularls.lua
+++ .config/nvim/lsp/angularls.lua
@@ -1,6 +1,6 @@
 -- Requires `ngserver` on PATH (e.g. npm i -g @angular/language-server).
 return {
 	cmd = { "ngserver", "--stdio" },
-	filetypes = { "html", "typescript", "typescriptreact" },
+	filetypes = { "html", "typescript", "typescriptreact", "htmlangular" },
 	root_markers = { "angular.json", "nx.json", "project.json", ".git" },
 }
